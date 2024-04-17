865109a1e18e17d7be3d715eb92a1e815da35a44
liujia <1595040768@qq.com>
2024-04-17 10:40:56 +0800

    增加分支合并和删除记录

--- git/14.分支branch基本管理操作.php
+++ git/14.分支branch基本管理操作.php
@@ -1,4 +1,17 @@
+分支的创建与切换
+
 git branch —— 查看仓库分支
 git branch 分支名  —— 创建一个新分支
 git checkout 分支名 —— 切换成对应分支
+git checkout -b 分支名 —— 创建一个分支，并且切换进入
+
+分支的合并与删除
+
+首先切换到主分支master
+
+git merge 分支名  —— 合并分支到主分支
+git branch -d 分支名 —— 删除分支
+
+处理分支冲突
+
 
